75a3bc32d0932b3d8f7702f952c9e7b2e728dbe6
santiagoQ <santiagoquinteroperfil@gmail.com>
2023-05-27 19:37:40 -0400

    Se sube proyecto, falta establecer la lógica para poder guardar las tareas seleccionadas

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react'
 import ButtonCheck from './ButtonCheck/ButtonCheck'
 import './Task.css'
 
-function Task ({ children, check }) {
+function Task ({ children, check, setSave }) {
   const [isCheck, setIsCheck] = useState(check)
 
   const classIsCheck = isCheck ? 'isCheck' : ''
   return (
     <div className='container-task'>
-      <ButtonCheck setIsCheck={setIsCheck} isCheck={isCheck}/>
+      <ButtonCheck setIsCheck={setIsCheck} isCheck={isCheck} />
       <span className={`text-task ${classIsCheck} `}>{children}</span>
       <button className='change-button'></button>
     </div>
