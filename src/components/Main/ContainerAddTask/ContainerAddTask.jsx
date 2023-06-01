a2447fa15563de763b351b24a54034fd13ebd9e7
santiagoQ <santiagoquinteroperfil@gmail.com>
2023-06-01 12:03:14 -0400

    Se agregan estilos finales y se cambia title e icon de la pag

--- src/components/Main/ContainerAddTask/ContainerAddTask.jsx
+++ src/components/Main/ContainerAddTask/ContainerAddTask.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import './ContainerAddTask.css'
 
 function ContainerAddTask ({ children }) {
-  return <aside className='container-addTask'>{children}</aside>
+  return (
+    <aside className='container-addTask'>
+      {children}
+      <img
+        src='../../../../public/arrowUp.svg'
+        className='giff'
+      ></img>
+    </aside>
+  )
 }
 
 export { ContainerAddTask }
